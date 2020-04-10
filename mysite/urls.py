4e38da098385c3d6db7804e20792bef9698574d1
Abhinav10 <abhinavkrjha10@gmail.com>
2020-04-10 19:25:24 +0530

    Added urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # post views
+    #url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
 ]
