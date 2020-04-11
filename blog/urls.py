eb7278374ffabb96d325bd4db64405320d764fe9
Abhinav10 <abhinavkrjha10@gmail.com>
2020-04-11 12:39:28 +0530

    introduced class based views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    #url(r'^$', views.post_list, name='post_list'),
-    url(r'^$',views.post_list, name='post_list'),
+    # url(r'^$',views.post_list, name='post_list'), commented so as to add class based views
+    url(r'^$',views.PostListView.as_view(),name ='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',views.post_detail,name='post_detail'),
 ]
