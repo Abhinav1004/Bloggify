b6c584f06bc9d047e2cf48a02a3c338f57e4acec
Abhinav10 <abhinavkrjha10@gmail.com>
2020-04-14 06:12:05 +0530

    Added email sharing

--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,9 @@ from .models import Post
 # 		posts = paginator.page(paginator.num_pages)
 # 	return render(request,'blog/post/list.html',{'page':page,'posts':posts})
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
 	queryset = Post.published.all()
@@ -27,6 +30,30 @@ class PostListView(ListView):
 
 
 def post_detail(request,year,month,day,post):
-	post = get_object_or_404(Post,slug = post,status='published',publish__year = year,publish__month=month,publish__day=day)
+	post = get_object_or_404(Post,slug = post,
+		status='published',
+		publish__year = year,
+		publish__month=month,
+		publish__day=day)
 	return render(request,'blog/post/detail.html',{'post':post})
 
+def post_share(request,post_id):
+	# retrieve post by id
+	post = get_object_or_404(Post,id = post_id,status='published')
+	sent = False
+	if request.method=='POST':
+		#form was submitted
+		print("Passed")
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			# form fields passed validation
+			cd = form.cleaned_data # is a dictionary containing the various fields
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'],cd['email'],post.title)
+			message = 'Read "{}" at {}\n\n{} comments:{}'.format(post.title,post_url,cd['name'],cd['comments'])
+			send_mail(subject,message,'abhinavkrjha10@gmail.com',[cd['to']])
+			sent = True
+	else:
+		print("Hello")
+		form  = EmailPostForm()
+	return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
